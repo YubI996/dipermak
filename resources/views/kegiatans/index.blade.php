aac7bf47caace6f4bcfdf69798099a7ac5459002
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-01-03 08:49:09 +0800

    perbaikan halaman index kegiatan

--- resources/views/kegiatans/index.blade.php
+++ resources/views/kegiatans/index.blade.php
@@ -2,24 +2,33 @@
 
 @section('content')
     <section class="content-header">
-        <div class="row mb-2 d-flex">
-         <div class="mr-auto p-2">
-            <h1>Kegiatan</h1></div>
-        <div class="p-2">
-            <h1>
-           <a class="btn btn-primary" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('kegiatans.create') }}">Add New</a>
-            </h1></div>
-    </div>
     </section>
     <div class="content">
         <div class="clearfix"></div>
-
+        
         @include('flash::message')
-
+        
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
-                    @include('kegiatans.table')
+                <div class="card card-default">
+                    <div class="card-header">
+                  <div class="d-flex">
+                   <div class="mr-auto p-2">
+                      <h1>Kegiatan</h1></div>
+                  <div class="p-2">
+                      <h1>
+                     <a class="btn btn-primary" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('kegiatans.create') }}">Add New</a>
+                      </h1></div>
+                  </div>
+              </div>
+              <!-- /.card-header -->
+            <div class="card-body">
+                @include('kegiatans.table')
+            </div>
+            <!-- /.card-body -->
+        </div>
+        {{-- datatable ex end --}}
             </div>
         </div>
         <div class="text-center">
