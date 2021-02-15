f839792a28d947888f69f558ebeca6e87693adb7
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-02-15 08:53:50 +0800

    penyederhanaan userfactory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,17 +7,24 @@ use Faker\Generator as Faker;
 
 $factory->define(user::class, function (Faker $faker) {
 
+    // return [
+    //     'name' => $faker->word,
+    //     'email' => $faker->email,
+    //     'email_verified_at' => $faker->word,
+    //     'password' => $faker->word,
+    //     'role_id' => $faker->randomDigitNotNull,
+    //     'rt_id' => $faker->randomDigitNotNull,
+    //     'foto' => $faker->word,
+    //     'remember_token' => $faker->word,
+    //     'created_at' => $faker->date('Y-m-d H:i:s'),
+    //     'updated_at' => $faker->date('Y-m-d H:i:s'),
+    //     'deleted_at' => $faker->date('Y-m-d H:i:s')
+    // ];
     return [
-        'name' => $faker->word,
-        'email' => $faker->email,
-        'email_verified_at' => $faker->word,
-        'password' => $faker->word,
-        'role_id' => $faker->randomDigitNotNull,
-        'rt_id' => $faker->randomDigitNotNull,
-        'foto' => $faker->word,
-        'remember_token' => $faker->word,
-        'created_at' => $faker->date('Y-m-d H:i:s'),
-        'updated_at' => $faker->date('Y-m-d H:i:s'),
-        'deleted_at' => $faker->date('Y-m-d H:i:s')
+        'name' => 'Yubi',
+            'email' => 'yubi@yubi.com',
+            'password' => Hash::make('yubiyubi'),
+            'role_id' => 1,
+            'rt_id' => 75,
     ];
 });
