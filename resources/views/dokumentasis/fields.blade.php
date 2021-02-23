d5a84770457d157134c7ad80dc6ff0e1dd2d5d68
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-02-23 12:25:05 +0800

    tambah component pilihrt di dokumentasi

--- resources/views/dokumentasis/fields.blade.php
+++ resources/views/dokumentasis/fields.blade.php
@@ -1,17 +1,22 @@
+@if (isset($dokumentasi))
+    @livewire('admin.pilihrt',['rtid'=>$dokumentasi->kegiatan->rt_id])
+@else
+    @livewire('admin.pilihrt')
+@endif
 <!-- Keg Id Field -->
-<div class="form-group col-sm-6">
+<div class="form-group row-sm-12">
     {!! Form::label('keg_id', 'Keg Id:') !!}
     {!! Form::select('keg_id', $kegiatanItems, Request::old('kegiatan_id'), ['class' => 'form-control','placeholder'=>'Pilih Kegiatan']) !!}
 </div>
 
 <!-- Rt Id Field -->
-{{-- <div class="form-group col-sm-6"> --}}
+{{-- <div class="form-group row-sm-12"> --}}
     {{-- {!! Form::label('rt_id', 'Rt Id:') !!} --}}
     {!! Form::hidden('rt_id', Auth::user()->rt_id) !!}
 {{-- </div> --}}
 
 <!-- Foto Field -->
-<div class="form-group col-sm-6">
+<div class="form-group row-sm-12">
     {!! Form::label('foto', 'Foto:') !!}
     @if (isset($dokumentasi->id))
         
@@ -22,13 +27,13 @@
 <div class="clearfix"></div>
 
 <!-- Keterangan Field -->
-<div class="form-group col-sm-12 col-lg-12">
+<div class="form-group row-sm-12">
     {!! Form::label('keterangan', 'Keterangan:') !!}
     {!! Form::textarea('keterangan', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Submit Field -->
-<div class="form-group col-sm-12">
+<div class="form-group row-sm-12">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
     <a href="{{ route('dokumentasis.index') }}" class="btn btn-default">Cancel</a>
 </div>
