30b505c6e94cfa6a5334038e9220b3275749fd28
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-02-23 12:28:34 +0800

    perbaikan tampilan show fields jenkeg

--- resources/views/jen_kegs/show_fields.blade.php
+++ resources/views/jen_kegs/show_fields.blade.php
@@ -1,18 +1,42 @@
-<!-- Jenis Keg Field -->
-<div class="form-group">
-    {!! Form::label('jenis_keg', 'Jenis Keg:') !!}
-    <p>{{ $jenKeg->jenis_keg }}</p>
-</div>
+<div class="col-lg-12">
+    <div class="form-group">
+        <div class="row-lg-12">
+            <div class="col-lg-2">
+                {!! Form::label('jenis_keg', 'Jenis Keg:') !!}
+            </div>
+            <div class="col-lg-10">
+                <!-- Jenis Keg Field -->
+                <p>{{ $jenKeg->jenis_keg }}</p>
+            </div>
+        </div>
+    </div>
+            
+    
+    <div class="row-lg-12">
+    <div class="form-group">
+        <div class="row-lg-12">
+            <div class="col-lg-2">
+        <!-- Created At Field -->
+            {!! Form::label('created_at', 'Created At:') !!}
+            </div>
+            <div class="col-lg-10">
 
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{{ $jenKeg->created_at }}</p>
-</div>
+                <p>{{ $jenKeg->created_at }}</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row-lg-12">
+    <div class="form-group">
+        <div class="row-lg-12">
+            <div class="col-lg-2">
+        <!-- Updated At Field -->
+            {!! Form::label('updated_at', 'Updated At:') !!}
+            </div>
+            <div class="col-lg-10">
 
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{{ $jenKeg->updated_at }}</p>
+                <p>{{ $jenKeg->updated_at }}</p>
+            </div>
+        </div>
+    </div>
 </div>
-
