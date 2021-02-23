6c30fc09a20161253a49d85f4f28cf3963cc17b4
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-02-23 14:59:16 +0800

    tambah kolom sumber dana dan satuan volume

--- resources/views/kegiatans/table.blade.php
+++ resources/views/kegiatans/table.blade.php
@@ -3,10 +3,11 @@
             <thead>
                 <tr>
                     <th>No.</th>
-                    <th class="align-middle">Nama Kegiatan</th>
                     <th class="align-middle">RT</th>
+                    <th class="align-middle">Nama Kegiatan</th>
                     <th class="align-middle">Tanggal Mulai</th>
                     <th class="align-middle">Tanggal Selesai</th>
+                    <th class="align-middle">Sumber Dana</th>
                     <th class="align-middle">Approval</th>
                     <th class="align-middle">Jenis Kegiatan</th>
                     <th class="align-middle">Pagu</th>
@@ -17,13 +18,14 @@
                 </tr>
             </thead>
             <tbody>
-            @foreach($kegiatans as $kegiatan)
+            @forelse($kegiatans as $kegiatan)
                 <tr>
                     <td>{{$loop->iteration}}</td>
-                    <td>{{ substr($kegiatan->nama_keg,0, 30).'...' }}</td>
                     <td>{{ 'RT '.$kegiatan->rt->nama_rt.' Kelurahan '.$kegiatan->rt->kelurahan->nama_kel }}</td>
+                    <td>{{ substr($kegiatan->nama_keg,0, 30).'...' }}</td>
                     <td>{{  \Carbon\Carbon::parse($kegiatan->tgl_mulai)->translatedFormat('l, d F Y')  }}</td>
                     <td>{{ \Carbon\Carbon::parse($kegiatan->tgl_selesai)->translatedFormat('l, d F Y')  }}</td>
+                    <td>{{ $kegiatan->sumber_dana }}</td>
                     <td>{{ $kegiatan->approval == "1" ? 'Disetujui' : 'Belum Disetujui' }}</td>
                     <td>{{ $kegiatan->jenKeg->jenis_keg }}</td>
                     <td>{{ 'Rp '.number_format($kegiatan->pagu,2,',','.') }}</td>
@@ -32,7 +34,7 @@
                         $p = $kegiatan->target/$kegiatan->pagu*100;
                     @endphp
                     <td>{{ number_format($p,2,',','.').'%' }}</td>
-                    <td>{{ $kegiatan->volume }}</td>
+                    <td>{{ $kegiatan->volume.' '.$kegiatan->satuan }}</td>
                     <td>
                         {!! Form::open(['route' => ['kegiatans.destroy', $kegiatan->id], 'method' => 'delete']) !!}
                         <div class='btn-group'>
@@ -43,6 +45,8 @@
                         {!! Form::close() !!}
                     </td>
                 </tr>
+            @empty
+            
             @endforeach
             </tbody>
         </table>
