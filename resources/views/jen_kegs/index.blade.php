c21a8c4c58dc6ad167e22ede42e37640e0bcf613
Sayid Fatich Sholachuddin Alayyubi <sayidyubi28@gmail.com>
2021-02-23 12:26:27 +0800

    edit tampilan index jenkeg

--- resources/views/jen_kegs/index.blade.php
+++ resources/views/jen_kegs/index.blade.php
@@ -1,26 +1,23 @@
 @extends('mold.app')
-
+@section('content-title')
+Jenis Kegiatan
+@endsection
 @section('content')
-    <section class="content-header">
-        <h1 class="pull-left">Jen Kegs</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('jenKegs.create') }}">Add New</a>
-        </h1>
-    </section>
-    <div class="content">
-        <div class="clearfix"></div>
+        <div class="row">
+            <div class="col-md-3">
+            </div>
+            <div class="col-md-8"></div>
+            <div class="col-md-1">
 
-        @include('flash::message')
-
-        <div class="clearfix"></div>
-        <div class="box box-primary">
-            <div class="box-body">
-                    @include('jen_kegs.table')
+                <h1 class="pull-right">
+                    <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('jenKegs.create') }}">Add New</a>
+                </h1>
             </div>
         </div>
-        <div class="text-center">
-        
+        @include('flash::message')
+        <div class="row-lg-12">
+            @include('jen_kegs.table')
         </div>
-    </div>
+            
 @endsection
 
